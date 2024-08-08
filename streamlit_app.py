9902c182e46b68a83dd433dd60c1da9b298f63c6
adripenaranda <adriana_penaranda@outlook.com>
2024-08-08 11:54:04 -0400

    Initial commit: Add Streamlit Breast Cancer Prediction App

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,14 +86,13 @@ size_df = input_df[size_features]
 
 with col1:
     st.subheader('Size Features ')
-    fig, ax = plt.subplots(figsize=(10,6 ))  # Adjust the figsize to make the plot bigger
+    fig, ax = plt.subplots(figsize=(8, 4))  # Adjust the figsize to make the plot bigger
     sns.barplot(x=size_df.columns, y=size_df.iloc[0], ax=ax, palette='coolwarm')
     ax.set_xticklabels(wrap_labels(['Mean Radius (µm)', 'Mean Perimeter (µm)', 'Mean Area (µm²)', 
                                     'Worst Radius (µm)', 'Worst Perimeter (µm)', 'Worst Area (µm²)'], 10), 
-                       rotation=0, horizontalalignment='center', fontsize=16)
-    ax.set_yticklabels(ax.get_yticks(), fontsize=16)
-    ax.set_ylabel('Values (µm or µm²)', fontsize=18)
-    ax.set_xlabel('Features', fontsize=18)
+                       rotation=0, horizontalalignment='center', fontsize=14)
+    ax.set_ylabel('Values (µm or µm²)', fontsize=16)
+    ax.set_xlabel('Features', fontsize=16)
     st.pyplot(fig)
 
 # Visualization for other features
@@ -102,14 +101,13 @@ other_df = input_df[other_features]
 
 with col2:
     st.subheader('Other Features ')
-    fig, ax = plt.subplots(figsize=(10, 6))  # Adjust the figsize to make the plot bigger
+    fig, ax = plt.subplots(figsize=(8, 4))  # Adjust the figsize to make the plot bigger
     sns.barplot(x=other_df.columns, y=other_df.iloc[0], ax=ax, palette='viridis')
     ax.set_xticklabels(wrap_labels(['Mean Concavity', 'Mean Concave Points', 
                                     'Worst Concavity', 'Worst Concave Points'], 10), 
-                       rotation=0, horizontalalignment='center', fontsize=16)
-    ax.set_yticklabels(ax.get_yticks(), fontsize=16)
-    ax.set_ylabel('Dimensionless Values', fontsize=18)
-    ax.set_xlabel('Features', fontsize=18)
+                       rotation=0, horizontalalignment='center', fontsize=14)
+    ax.set_ylabel('Dimensionless Values', fontsize=16)
+    ax.set_xlabel('Features', fontsize=16)
     st.pyplot(fig)
     
     
